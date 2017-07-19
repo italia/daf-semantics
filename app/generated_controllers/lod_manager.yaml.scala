7a5fd5c233955afd393a3332a6421943e8c9a609
Alfredo Serafini <seralf@gmail.com>
2017-07-19 14:41:38 +0200

    updated controllers

--- app/generated_controllers/lod_manager.yaml.scala
+++ app/generated_controllers/lod_manager.yaml.scala
@@ -1,11 +1,11 @@
 
-import play.api.mvc.{Action,Controller}
+import play.api.mvc.{ Action, Controller }
 
 import play.api.data.validation.Constraint
 
 import play.api.i18n.MessagesApi
 
-import play.api.inject.{ApplicationLifecycle,ConfigurationProvider}
+import play.api.inject.{ ApplicationLifecycle, ConfigurationProvider }
 
 import de.zalando.play.controllers._
 
@@ -69,6 +69,21 @@ import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
 
 /**
  * This controller is re-generated after each change in the specification.
@@ -76,37 +91,36 @@ import play.api.libs.concurrent.Execution.Implicits.defaultContext
  */
 
 package lod_manager.yaml {
-    // ----- Start of unmanaged code area for package Lod_managerYaml
-                        
+  // ----- Start of unmanaged code area for package Lod_managerYaml
+
   import play.api.libs.concurrent.Execution.Implicits.defaultContext
 
-    // ----- End of unmanaged code area for package Lod_managerYaml
-    class Lod_managerYaml @Inject() (
-        // ----- Start of unmanaged code area for injections Lod_managerYaml
+  // ----- End of unmanaged code area for package Lod_managerYaml
+  class Lod_managerYaml @Inject() (
+      // ----- Start of unmanaged code area for injections Lod_managerYaml
 
       kb: KBModuleBase,
 
-        // ----- End of unmanaged code area for injections Lod_managerYaml
-        val messagesApi: MessagesApi,
-        lifecycle: ApplicationLifecycle,
-        config: ConfigurationProvider
-    ) extends Lod_managerYamlBase {
-        // ----- Start of unmanaged code area for constructor Lod_managerYaml
+      // ----- End of unmanaged code area for injections Lod_managerYaml
+      val messagesApi: MessagesApi,
+      lifecycle: ApplicationLifecycle,
+      config: ConfigurationProvider) extends Lod_managerYamlBase {
+    // ----- Start of unmanaged code area for constructor Lod_managerYaml
 
-        // ----- End of unmanaged code area for constructor Lod_managerYaml
-        val countTriples = countTriplesAction {  _ =>  
-            // ----- Start of unmanaged code area for action  Lod_managerYaml.countTriples
-            val kbrepo = kb.kbrepo
+    // ----- End of unmanaged code area for constructor Lod_managerYaml
+    val countTriples = countTriplesAction { _ =>
+      // ----- Start of unmanaged code area for action  Lod_managerYaml.countTriples
+      val kbrepo = kb.kbrepo
       lazy val triples = kbrepo.triplesCount()
 
       CountTriples200(Future {
         TriplesCount("foaf", triples)
       })
-            // ----- End of unmanaged code area for action  Lod_managerYaml.countTriples
-        }
-        val countTriplesByOntology = countTriplesByOntologyAction { (ontoid: String) =>  
-            // ----- Start of unmanaged code area for action  Lod_managerYaml.countTriplesByOntology
-            val kbrepo = kb.kbrepo
+      // ----- End of unmanaged code area for action  Lod_managerYaml.countTriples
+    }
+    val countTriplesByOntology = countTriplesByOntologyAction { (ontoid: String) =>
+      // ----- Start of unmanaged code area for action  Lod_managerYaml.countTriplesByOntology
+      val kbrepo = kb.kbrepo
       //      lazy val triples = kbrepo.triplesCount("http://xmlns.com/foaf/spec/index.rdf") // TODO: add an inverse prefix lookup
 
       lazy val triples = kbrepo.triplesCount("memory://graph/") // TODO: add an inverse prefix lookup
@@ -116,11 +130,11 @@ package lod_manager.yaml {
       })
 
       //      NotImplementedYet
-            // ----- End of unmanaged code area for action  Lod_managerYaml.countTriplesByOntology
-        }
-        val getOntology = getOntologyAction { (slug: String) =>  
-            // ----- Start of unmanaged code area for action  Lod_managerYaml.getOntology
-            //      val onto = ServiceRegistry.kbRepository.getOntology(slug)
+      // ----- End of unmanaged code area for action  Lod_managerYaml.countTriplesByOntology
+    }
+    val getOntology = getOntologyAction { (slug: String) =>
+      // ----- Start of unmanaged code area for action  Lod_managerYaml.getOntology
+      //      val onto = ServiceRegistry.kbRepository.getOntology(slug)
 
       val kbrepo = kb.kbrepo
 
@@ -136,19 +150,19 @@ package lod_manager.yaml {
 
       val result = kbrepo.execute_query(query) // TEST - TODO: add a properrepresentation
       GetOntology200(result.toList.mkString("\n"))
-            // ----- End of unmanaged code area for action  Lod_managerYaml.getOntology
-        }
-        val getCurrentTime = getCurrentTimeAction {  _ =>  
-            // ----- Start of unmanaged code area for action  Lod_managerYaml.getCurrentTime
-            val now = new Date()
+      // ----- End of unmanaged code area for action  Lod_managerYaml.getOntology
+    }
+    val getCurrentTime = getCurrentTimeAction { _ =>
+      // ----- Start of unmanaged code area for action  Lod_managerYaml.getCurrentTime
+      val now = new Date()
 
       Logger.info(s"${this.HOST}/time/now: ${now}")
 
       GetCurrentTime200(Future {
         Time(1111, now.toString)
       })
-            // ----- End of unmanaged code area for action  Lod_managerYaml.getCurrentTime
-        }
-    
+      // ----- End of unmanaged code area for action  Lod_managerYaml.getCurrentTime
     }
+
+  }
 }
