cb5bf9a8395627dbc8219ec21fcbe90012ac8d0a
Alfredo Serafini <seralf@gmail.com>
2017-07-26 14:26:51 +0200

    added idea for metadata handler on file

--- test/it/almawave/kb/FileDatastoreTest.scala
+++ test/it/almawave/kb/FileDatastoreTest.scala
@@ -2,19 +2,47 @@ package it.almawave.kb
 
 import org.junit.Assert
 import org.junit.Test
+import java.net.URI
+import java.nio.file.Paths
+import java.nio.file.Files
+import scala.io.Source
+import com.typesafe.config.ConfigFactory
+
+import scala.collection.JavaConversions._
+import scala.collection.JavaConverters._
 
 class FileDatastoreTest {
 
+  val fs = new FileDatastore("ontologies")
+
   @Test
   def test_local_relative_files {
-    var fs = new FileDatastore("ontologies")
     Assert.assertTrue(fs.list("owl", "ttl").size > 0)
   }
 
-  //  @Test DISABLED
-  def test_remote_files {
-    var fs = new FileDatastore("http://xmlns.com/foaf/spec/index.rdf")
-    Assert.assertTrue(fs.list("rdf").size > 0)
+  @Test
+  def test_local_uri() {
+    val foaf_local_uri = new URI("file:///C:/Users/Al.Serafini/repos/DAF/lod_manager/ontologies/foaf/foaf.rdf")
+    val list = fs.list("owl", "ttl", "rdf")
+    Assert.assertTrue(list.contains(foaf_local_uri))
+  }
+
+  @Test
+  def test_metadata {
+    val list = fs.list("owl", "ttl", "rdf")
+
+    // trying to get metadata, with error
+    val uri_no = new URI("file:///not/existing/path/file.rdf")
+    val meta_no = fs.getMetadata(uri_no)
+    Assert.assertEquals(ConfigFactory.empty(), meta_no)
+
+    // get metadata
+    val uri = new URI("file:///C:/Users/Al.Serafini/repos/DAF/lod_manager/ontologies/foaf/foaf.rdf")
+    val meta = fs.getMetadata(uri)
+    Assert.assertEquals("foaf", meta.getString("prefix"))
+    Assert.assertEquals("http://xmlns.com/foaf/0.1/", meta.getString("uri"))
+    Assert.assertTrue(meta.getList("contexts").unwrapped().contains("http://xmlns.com/foaf/0.1/"))
+
   }
 
 }
