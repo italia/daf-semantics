aee0b62d706132b9f31879f25c7bbf61dc98ec00
Alfredo Serafini <seralf@gmail.com>
2017-11-09 15:14:55 +0100

    updating on latest versions, preparing merge

--- semantic_standardization/app/generated_controllers/semantic_standardization.yaml.scala
+++ semantic_standardization/app/generated_controllers/semantic_standardization.yaml.scala
@@ -1,11 +1,11 @@
 
-import play.api.mvc.{ Action, Controller }
+import play.api.mvc.{Action,Controller}
 
 import play.api.data.validation.Constraint
 
 import play.api.i18n.MessagesApi
 
-import play.api.inject.{ ApplicationLifecycle, ConfigurationProvider }
+import play.api.inject.{ApplicationLifecycle,ConfigurationProvider}
 
 import de.zalando.play.controllers._
 
@@ -17,6 +17,15 @@ import scala.util._
 
 import javax.inject._
 
+import play.api.mvc.{Action,Controller}
+import play.api.data.validation.Constraint
+import play.api.i18n.MessagesApi
+import play.api.inject.{ApplicationLifecycle,ConfigurationProvider}
+import de.zalando.play.controllers._
+import PlayBodyParsing._
+import PlayValidations._
+import scala.util._
+import javax.inject._
 import java.io.File
 import modules.KBModuleBase
 import play.api.libs.ws.WSClient
@@ -28,29 +37,30 @@ import play.api.libs.concurrent.Execution.Implicits.defaultContext
  */
 
 package semantic_standardization.yaml {
-  // ----- Start of unmanaged code area for package Semantic_standardizationYaml
-
-  // ----- End of unmanaged code area for package Semantic_standardizationYaml
-  class Semantic_standardizationYaml @Inject() (
-      // ----- Start of unmanaged code area for injections Semantic_standardizationYaml
+    // ----- Start of unmanaged code area for package Semantic_standardizationYaml
+    
+    // ----- End of unmanaged code area for package Semantic_standardizationYaml
+    class Semantic_standardizationYaml @Inject() (
+        // ----- Start of unmanaged code area for injections Semantic_standardizationYaml
 
       kb: KBModuleBase,
       ws: WSClient,
 
-      // ----- End of unmanaged code area for injections Semantic_standardizationYaml
-      val messagesApi: MessagesApi,
-      lifecycle: ApplicationLifecycle,
-      config: ConfigurationProvider) extends Semantic_standardizationYamlBase {
-    // ----- Start of unmanaged code area for constructor Semantic_standardizationYaml
+        // ----- End of unmanaged code area for injections Semantic_standardizationYaml
+        val messagesApi: MessagesApi,
+        lifecycle: ApplicationLifecycle,
+        config: ConfigurationProvider
+    ) extends Semantic_standardizationYamlBase {
+        // ----- Start of unmanaged code area for constructor Semantic_standardizationYaml
 
     // wrapper for triplestore
     val kbrepo = kb.kbrepo
 
-    // ----- End of unmanaged code area for constructor Semantic_standardizationYaml
-    val propertiesHierarchyList = propertiesHierarchyListAction { input: (String, String, String) =>
-      val (vocabulary_name, ontology_name, lang) = input
-      // ----- Start of unmanaged code area for action  Semantic_standardizationYaml.propertiesHierarchyList
-      val parameters = Map("lang" -> lang)
+        // ----- End of unmanaged code area for constructor Semantic_standardizationYaml
+        val propertiesHierarchyList = propertiesHierarchyListAction { input: (String, String, String) =>
+            val (vocabulary_name, ontology_name, lang) = input
+            // ----- Start of unmanaged code area for action  Semantic_standardizationYaml.propertiesHierarchyList
+            val parameters = Map("lang" -> lang)
       val vocapi = kb.vocabularyAPI.items(vocabulary_name)
       val ontoapi = kb.ontologyAPI.items(ontology_name)
 
@@ -76,12 +86,12 @@ package semantic_standardization.yaml {
           case ex: Throwable =>
             PropertiesHierarchyList500(Error(s"cannot obtain property hierarchies for ${ontology_name}/${vocabulary_name} ", ex.getMessage))
         }
-      // ----- End of unmanaged code area for action  Semantic_standardizationYaml.propertiesHierarchyList
-    }
-    val vocabularyDataset = vocabularyDatasetAction { input: (String, String) =>
-      val (name, lang) = input
-      // ----- Start of unmanaged code area for action  Semantic_standardizationYaml.vocabularyDataset
-      val vocapi = kb.vocabularyAPI.items(name)
+            // ----- End of unmanaged code area for action  Semantic_standardizationYaml.propertiesHierarchyList
+        }
+        val vocabularyDataset = vocabularyDatasetAction { input: (String, String) =>
+            val (name, lang) = input
+            // ----- Start of unmanaged code area for action  Semantic_standardizationYaml.vocabularyDataset
+            val vocapi = kb.vocabularyAPI.items(name)
       val tree = vocapi.extract_data(Map("lang" -> "it"))
 
       val items = tree
@@ -96,8 +106,8 @@ package semantic_standardization.yaml {
           case ex: Throwable =>
             VocabularyDataset500(Error(s"problems obtaining a flat representation for Vocabulary ${name}", ex.getMessage))
         }
-      // ----- End of unmanaged code area for action  Semantic_standardizationYaml.vocabularyDataset
+            // ----- End of unmanaged code area for action  Semantic_standardizationYaml.vocabularyDataset
+        }
+    
     }
-
-  }
 }
